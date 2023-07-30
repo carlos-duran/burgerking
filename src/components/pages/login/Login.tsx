a636e5f4432b8d236d49bf5e448251b961fee7ad
Carlos Duran <carlosduran.xyz@gmail.com>
2023-07-30 01:22:28 -0500

    add login form (wip)

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,3 +1,56 @@
+import { useNotify } from "../../../context/Notify";
+
 export const Login = () => {
-  return <div>Login</div>;
+  const notify = useNotify();
+
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+
+    const payload = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    try {
+      const response = await fetch("/login", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+      if (response.ok) {
+        notify("Bienvenido!");
+      } else {
+        // correo o contraseña inválidos
+      }
+    } catch (error) {
+      // no se pudo establecer conexión con el servidor
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={() => notify("Hola mundo cruel!")}>Notify</button>
+      <button onClick={() => notify("Hola mundo sad!")}>Notify 2</button>
+      <button onClick={() => notify("Hola mundo mal!")}>Notify 3</button>
+      <button onClick={() => notify("Hola mundito!")}>Notify 4</button>
+
+      <form onSubmit={onSubmit}>
+        <label className="block">
+          <span>Correo</span>
+          <input type="email" name="email" />
+        </label>
+
+        <label className="block">
+          <span>Contraseña</span>
+          <input type="password" name="password" />
+        </label>
+
+        <button>Iniciar sesión</button>
+      </form>
+    </div>
+  );
 };
